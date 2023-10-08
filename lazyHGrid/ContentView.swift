dc8914c63d1230ca6f379548286a51fbaed933ac
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-08 17:46:24 +0900

    .gitignoreとlazyHGrid

--- lazyHGrid/ContentView.swift
+++ lazyHGrid/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  lazyHGrid
+//  lazyVGrid
 //
 //  Created by emi oiso on 2023/10/08.
 //
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    let symbols = [
+        "pencil",
+        "pencil.circle",
+        "pencil.circle.fill",
+        "pencil.slash",
+        "square.and.pencil",
+        "rectangle.and.pencil.and.ellipsis",
+        "highlighter",
+        "pencil.and.outline",
+        "pencil.tip",
+        "pencil.tip.crop.circle",
+        "pencil.tip.crop.circle.badge.plus",
+        "pencil.tip.crop.circle.badge.minus",
+        "pencil.tip.crop.circle.badge.arrow.forward",
+        "lasso",
+        "lasso.badge.sparkles",
+        "trash",
+        "trash.fill",
+        "trash.circle",
+        "trash.circle.fill",
+        "trash.square",
+        "trash.square.fill",
+        "trash.slash",
+    ]
+    let rows: [GridItem] = Array(repeating: .init(.flexible()), count: 1)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: rows) {
+                ForEach(symbols,id: \.self) { symbol in
+                    Image(systemName: symbol)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50,height: 50)
+                        .padding(20)
+                        .background(.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
         }
+        .frame(height:130)
+        .background(.gray)
+        .cornerRadius(10)
         .padding()
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    
+    #Preview {
+        ContentView()
+    }
