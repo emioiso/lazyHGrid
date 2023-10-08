43275ad97118e6659c31bfc5fbe3eff028543816
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-08 17:23:41 +0900

    Initial Commit

---
+++ lazyHGrid/lazyHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazyHGridApp.swift
+//  lazyHGrid
+//
+//  Created by emi oiso on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct lazyHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
